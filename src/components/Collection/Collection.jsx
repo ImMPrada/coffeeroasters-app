62657b7e5f50b10b10ffb88bf0fab5f2fb882e87
miguel prada <im.mprada@gmail.com>
2023-11-03 18:01:24 -0500

    add styles

--- src/components/Collection/Collection.jsx
+++ src/components/Collection/Collection.jsx
@@ -4,31 +4,35 @@ import './styles.scss'
 function Collection() {
   return (
     <>
-      <div className="collection-container">
-        <div className="collection-container-title">
-          
-        </div>
-        <div className="collection-container-cards">
-          <CollectionCard
-            imagePath = "src/assets/danche.png"
-            title = "Gran Espresso"
-            description = "Light and flavorful blend with cocoa and black pepper for an intense experience"
-          />
-          <CollectionCard
-            imagePath = "src/assets/planalto.png"
-            title = "Planalto"
-            description = "Brazilian dark roast with rich and velvety body, and hints of fruits and nuts"
-          />
-          <CollectionCard
-            imagePath = "src/assets/picollo.png"
-            title = "Picollo"
-            description = "Mild and smooth blend featuring notes of toasted almond and dried cherry"
-          />
-          <CollectionCard
-            imagePath = "src/assets/danche.png"
-            title = "Danche"
-            description = "Ethiopian hand-harvested blend densely packed with vibrant fruit notes"
-          />
+      <div className="collection">
+        <div className="collection-container">
+          <div className="collection-container-title">
+            <p className="big-title-font">
+              our collection
+            </p>
+          </div>
+          <div className="collection-container-cards">
+            <CollectionCard
+              imagePath = "src/assets/danche.png"
+              title = "Gran Espresso"
+              description = "Light and flavorful blend with cocoa and black pepper for an intense experience"
+            />
+            <CollectionCard
+              imagePath = "src/assets/planalto.png"
+              title = "Planalto"
+              description = "Brazilian dark roast with rich and velvety body, and hints of fruits and nuts"
+            />
+            <CollectionCard
+              imagePath = "src/assets/picollo.png"
+              title = "Picollo"
+              description = "Mild and smooth blend featuring notes of toasted almond and dried cherry"
+            />
+            <CollectionCard
+              imagePath = "src/assets/danche.png"
+              title = "Danche"
+              description = "Ethiopian hand-harvested blend densely packed with vibrant fruit notes"
+            />
+          </div>
         </div>
       </div>
     </>
