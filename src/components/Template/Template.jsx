fa4d718ed7eb3f05ce7012cc3a44c286174c52aa
miguel prada <im.mprada@gmail.com>
2023-11-10 18:26:07 -0500

    add footing to template

--- src/components/Template/Template.jsx
+++ src/components/Template/Template.jsx
@@ -1,6 +1,9 @@
 import './styles.scss'
-import Bean from '../../assets/Bean.svg'
+import logo from '../../assets/logo.svg'
 import NavBar from '../NavBar/NavBar.jsx'
+import Footing from '../Footing/Footing.jsx'
+
+const title = "Coffeeroasters";
 
 function Template(props) {
   const { children } = props;
@@ -8,10 +11,14 @@ function Template(props) {
   return (
     <>
       <NavBar
-        title = "Coffeeroasters"
-        image = {Bean}
+        title = {title}
+        image = {logo}
       />
       {children}
+      <Footing
+        image = {logo}
+        title = {title}
+      />
     </>
   )
 }
